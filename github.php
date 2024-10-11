bd78336788cc1c8af3736897b31ce5fc91d794d3
Frikelangelo <mirius201400@gmail.com>
2024-10-11 16:19:47 +0200

    Este cambio añade un Hola

---
+++ github.php
@@ -0,0 +1,21 @@
+<html>
+
+    <head>
+
+    </head>
+    <body>
+        <?php
+        //git init
+        //git config --globar user.name "Frikelangelo"
+        //git config --global user.email "mirius201400@gmail.com"
+        //git remote add origin https://github.com/Frikelangelo/P_Taller.git
+        //git status
+        //git add prueba.html
+        //git add .
+
+Hola
+
+        ?>
+    </body>
+
+</html>
